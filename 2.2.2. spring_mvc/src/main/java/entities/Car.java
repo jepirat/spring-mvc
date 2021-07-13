a540721a47fbdbdc0d3171dc7f82ab941b07ac8c
Евгений <evkafanov@gmail.com>
2021-07-13 07:39:15 +0400

    плохая кодировка

--- 2.2.2. spring_mvc/src/main/java/entities/Car.java
+++ 2.2.2. spring_mvc/src/main/java/entities/Car.java
@@ -1,2 +1,39 @@
-package entities;public class Car {
+package entities;
+public class Car {
+    String name;
+    String speed;
+    String color;
+
+    public Car() {
+    }
+
+    public Car(String name, String speed, String color) {
+        this.name = name;
+        this.speed = speed;
+        this.color = color;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(String speed) {
+        this.speed = speed;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
 }
--- 2.2.2. spring_mvc/src/main/java/web/controller/CarsController.java
+++ 2.2.2. spring_mvc/src/main/java/web/controller/CarsController.java
@@ -1,2 +1,32 @@
-package web.controller;public class CarsController {
+package web.controller;
+
+import entities.Car;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Controller
+public class CarsController {
+    @GetMapping(value = "/cars")
+    public String printCars(Model model, @RequestParam(value = "quality", required = false) Integer quality) {
+        List<Car> cars = new ArrayList<>();
+        cars.add(new Car("Toyota", "200", "red"));
+        cars.add(new Car("Ferrari", "250", "blue"));
+        cars.add(new Car("Волга", "120", ",белая"));
+        cars.add(new Car("Москвич", "120", "синий"));
+        cars.add(new Car("Запорожец", "120", "красный"));
+        if (quality != null) {
+            List<Car> sortedCars = cars.stream().limit(quality).collect(Collectors.toList());
+            model.addAttribute("cars", sortedCars);
+            return "cars";
+        } else {model.addAttribute("cars", cars);
+            return "cars";
+        }
+    }
 }
