f4ce1ce8c6f8a39edf6d1e54ecf9e81857f8fbd2
Евгений <evkafanov@gmail.com>
2021-07-13 18:38:59 +0400

    еще более готовый вариант

--- 2.2.2. spring_mvc/src/main/java/web/controller/CarsController.java
+++ 2.2.2. spring_mvc/src/main/java/web/controller/CarsController.java
@@ -1,16 +1,10 @@
 package web.controller;
 
-import entities.Car;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import services.CarService;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.stream.Collectors;
 
 @Controller
 public class CarsController {
